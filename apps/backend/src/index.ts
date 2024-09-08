1d4f5f59a5a64e71f73605dc398cf49604ffe441
Juraj Zec <juro.zec.1@gmail.com>
2024-09-08 19:31:59 +0200

    formatting stream chunks as they are being emitted from the fibonacci generator

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream'
+import { Readable, Transform } from 'node:stream'
 import Fastify from 'fastify'
 import { fibonacci } from './utils/fibonacci'
 
@@ -7,16 +7,33 @@ const fastify = Fastify({
 })
 
 fastify.get('/stream', (_request, reply) => {
-  const stream = Readable.from(fibonacci())
+  const source = Readable.from(fibonacci(500))
+  const format = new Transform({
+    transform: (chunk, _encoding, callback) => {
+      let i = 0
+      callback(
+        null,
+        JSON.stringify({
+          seq: ++i,
+          data: String(chunk),
+          ts: new Date().toISOString(),
+        }),
+      )
+    },
+  })
 
-  stream.on('data', (chunk) => fastify.log.info(chunk))
-  stream.once('close', () => fastify.log.info('Stream closed.'))
+  const result = source.pipe(format)
 
-  reply.header('Content-Type', 'application/octet-stream').send(stream)
+  result.on('data', (chunk) => fastify.log.info(String(chunk)))
+  result.once('close', () => fastify.log.info('Stream closed.'))
+
+  reply.header('Access-Control-Allow-Origin', '*')
+  reply.header('Content-Type', 'application/octet-stream')
+  reply.send(result)
 })
 
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ port: 3001 })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
